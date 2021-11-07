de403402535c846c69633b0a1e531b063178c91f
Davis Arrizqi Putra Mandiri <85699695+davisapplication@users.noreply.github.com>
2021-11-07 23:40:19 +0700

    Pengurutan Nilai (Terbesar)
    Ini adalah script untuk menentukan mana angka yang paling besar, yang terdapat pada list angka. Pada kali ini saya menggunakan C++ sebagai bahasanya.

---
+++ nilaiTerbesar.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class PusatData{
+    public:
+    int angka[100]; long int jumlahAngka; long int angkaTerbesar;
+    
+    void selamatDatang(){
+        cout << "<<=== MATH BY DAVISAPP ===>>" << endl;
+    }
+    
+    void masukkanNilai(){
+        cout << "Jumlah Angka --> "; cin >> jumlahAngka; 
+        cout << endl; cout << "List Angka: " << endl;
+        
+        for(int a=0;a<jumlahAngka;a++){
+            if(a!=jumlahAngka-1){
+                cout << "> "; cin >> angka[a]; 
+            }
+            
+            else{
+                cout << "> "; cin >> angka[a]; cout << endl;
+            }
+        }
+        
+        cout << "Input Data Sukses!" << endl << "\n";
+    }
+    
+    void mulaiPencarian(){
+        for(int s=0;s<jumlahAngka;s++){
+            long int m1 = angka[s]; long int m2 = angka[0];
+            if(m1>m2){
+                angkaTerbesar = m1;
+            }
+        }
+        
+        cout << "Angka Terbesarnya --> " << angkaTerbesar << endl;
+    }
+};
+
+int main(){
+    PusatData done; done.selamatDatang(); done.masukkanNilai();
+    done.mulaiPencarian();
+}
